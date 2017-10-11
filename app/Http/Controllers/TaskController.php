db15d9520fe5d19d172d5ef9c29458692d76c125
foysaliuice <foysal.iuice@gmail.com>
2017-10-11 23:10:45 +0600

    Add session message to view

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Tasks;
+use Session;
 class TaskController extends Controller
 {
     public function index(){
@@ -15,17 +16,18 @@ class TaskController extends Controller
       $items = new Tasks();
       $items->task = $request->get('text');
       $items->save();
-
-      return "done";
+      Session::flash('status','Item Added');
     }
 
     public function delete(request $request){
       Tasks::where('id',$request->id)->delete();
+      Session::flash('status','Item deleted');
     }
 
     public function update(request $request){
       $items = Tasks::find($request->id);
       $items->task = $request->value;
       $items->save();
+      Session::flash('status','Item Updated');
     }
 }
